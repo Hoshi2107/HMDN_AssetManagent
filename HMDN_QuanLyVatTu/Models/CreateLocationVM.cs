806ca1563f6adb555ff367eb0dfdf570c4bb2960
Hoshi2107 <hoshi.kagayaku2008@gmail.com>
2026-05-19 11:18:37 +0700

    add func for Location include: create/update/toggle status, add modal for create/update/toggle status.

---
+++ HMDN_QuanLyVatTu/Models/CreateLocationVM.cs
@@ -0,0 +1,13 @@
+﻿namespace HMDN.Models.Location
+{
+    public class CreateLocationVM
+    {
+        public string Name { get; set; }
+
+        public string Floor { get; set; }
+
+        public string Building { get; set; }
+
+        public int? DepartmentId { get; set; }
+    }
+}
---
+++ HMDN_QuanLyVatTu/Models/UpdateLocation.cs
@@ -0,0 +1,15 @@
+﻿namespace HMDN.Models.Location
+{
+    public class UpdateLocationVM
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Floor { get; set; }
+
+        public string Building { get; set; }
+
+        public int? DepartmentId { get; set; }
+    }
+}
