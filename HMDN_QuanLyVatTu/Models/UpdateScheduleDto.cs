f6dfbdef373195c6c4a30f94c32859339be8d3f1
Hoshi2107 <hoshi.kagayaku2008@gmail.com>
2026-07-15 14:42:48 +0700

    add Edit button for Maintain List, Add modal that confirm the date for the next maintain schedule (can change date for loop schedule )

---
+++ HMDN_QuanLyVatTu/Models/UpdateScheduleDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+public class UpdateScheduleDto
+{
+    public int Id { get; set; }
+    public string ScheduleName { get; set; }
+    public DateTime NextMaintenanceDate { get; set; }
+    public int ReminderDays { get; set; }
+    public bool IsRecurring { get; set; }
+    public int? RecurringMonths { get; set; }
+}
